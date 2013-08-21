15cee96097cac6ad812094f4a20456867f28d1ed
dylanmei <dylanmei@gmail.com>
2013-08-21 10:35:46 -0700

    Begin migrating specs to Kikiri

---
+++ specs/EventSpec.cs
@@ -0,0 +1,14 @@
+﻿using EventToolkit;
+using Kekiri;
+
+namespace Specs
+{
+    public class EventSpec : ScenarioTest
+    {
+        [NUnit.Framework.TestFixtureTearDown]
+        public void Teardown()
+        {
+            EventBus.Clear();
+        }
+    }
+}
--- specs/Scopes.cs
+++ specs/Scopes.cs
@@ -1,31 +1,48 @@
-using System;
 using EventToolkit;
-using Machine.Specifications;
+using Kekiri;
+using FluentAssertions;
 
 namespace Specs
 {
-  [Subject("Scopes")]
-  public class When_using_a_nested_scope {
-    protected static ScopedEventBus localBus;
-    protected static GlobalEventBus globalBus = new GlobalEventBus();
-    protected static SimpleSubscriber nestedSubscriber = new SimpleSubscriber();
-    protected static SimpleSubscriber outerSubscriber = new SimpleSubscriber();
+    [Scenario("Scopes")]
+    public class When_raising_an_event_at_the_local_scope : EventSpec
+    {
+        protected ScopedEventBus localBus;
+        protected GlobalEventBus globalBus;
+        protected SimpleSubscriber nestedSubscriber = new SimpleSubscriber();
+        protected SimpleSubscriber outerSubscriber = new SimpleSubscriber();
 
-    Establish context = () => {
-      globalBus.Subscribe<Message>(outerSubscriber);
-      localBus = globalBus.CreateScope();
-    };
+        [Given]
+        public void given_a_handler_subscribed_at_an_outer_scope()
+        {
+            globalBus = new GlobalEventBus();
+            globalBus.Subscribe<EventMessage>(outerSubscriber);
+        }
 
-    Because of = () => {
-      using (localBus.Subscribe<Message>(nestedSubscriber))
-        localBus.Publish(new Message());
-    };
+        [Given]
+        public void given_a_handler_subscribed_at_the_local_scope()
+        {
+            localBus = globalBus.CreateScope();
+            localBus.Subscribe<EventMessage>(nestedSubscriber);
+        }
 
-    It should_send_an_event_to_handlers_subscribing_to_the_nested_scope = () => nestedSubscriber.handled.ShouldBeTrue();
-    It should_dispose_handlers_subscribing_to_the_nested_scope = () => nestedSubscriber.disposed.ShouldBeTrue();
+        [When]
+        public void when()
+        {
+            localBus.Publish(new EventMessage());
+        }
 
-    It should_send_an_event_to_handlers_subscribing_to_the_outer_scope = () => outerSubscriber.handled.ShouldBeTrue();
-    It should_not_dispose_handlers_subscribing_to_the_outer_scope = () => outerSubscriber.disposed.ShouldBeFalse();
-  }
+        [Then]
+        public void it_sends_an_event_to_handlers_at_the_nested_scope()
+        {
+            nestedSubscriber.handled.Should().BeTrue();
+        }
+
+        [Then]
+        public void it_sends_an_event_to_handlers_at_the_outer_scope()
+        {
+            outerSubscriber.handled.Should().BeTrue();
+        }
+    }
 }
 
